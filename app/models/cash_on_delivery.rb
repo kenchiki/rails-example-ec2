bb9667183f645a606fdebaf0291ea103029327c8
nagai kenji <nagai-kj@nagai-galaxy.com>
2019-04-11 17:59:29 +0900

    [add]見た目調整

--- app/models/cash_on_delivery.rb
+++ app/models/cash_on_delivery.rb
@@ -1,5 +1,5 @@
 class CashOnDelivery < ApplicationRecord
   has_many :cash_on_delivery_details, -> { order(more_than: :asc) },
            dependent: :destroy, inverse_of: :cash_on_delivery
-  accepts_nested_attributes_for :cash_on_delivery_details
+  accepts_nested_attributes_for :cash_on_delivery_details, reject_if: :all_blank
 end
