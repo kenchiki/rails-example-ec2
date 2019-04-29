9ec9e0abe2cf044ddb5d28553c660e86911ab122
nagai kenji <nagai-kj@nagai-galaxy.com>
2019-04-29 09:33:09 +0900

    [update]basic認証の仕組み

--- README.md
+++ README.md
@@ -9,3 +9,32 @@
 
 ## デモ用データ作成
 `rails sample:create_demo`
+
+# 本番環境で設定する環境変数
+```ruby
+# herokuでは○○.herokuapp.comを設定
+ACTION_MAILER_DOMAIN
+
+# herokuでは○○.herokuapp.comを設定
+ACTION_MAILER_HOST
+
+# herokuでは設定不要（未設定の場合、ENV['ACTION_MAILER_PORT']はnilになる）
+ACTION_MAILER_PORT
+
+# herokuのSENDGRIDプラグインで自動で設定される
+SENDGRID_USERNAME
+
+# herokuのSENDGRIDプラグインで自動で設定される
+SENDGRID_PASSWORD
+
+# awsの設定
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+AWS_REGION
+AWS_HOST
+AWS_ENDPOINT
+AWS_BUCKET
+
+# メールアドレス送信時のFROM
+EMAIL_FROM
+```
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  helper_method :current_cart
+
+  before_action :basic_authentication, :authenticate_user!
+
+  private
 
   def current_cart
     @current_cart ||= Cart.session_or_create(session)
@@ -9,5 +13,11 @@ class ApplicationController < ActionController::Base
     @current_cart = Cart.create_empty(session)
   end
 
-  helper_method :current_cart
+  def basic_authentication
+    return unless Rails.env.production?
+    basic_auth_username = ENV['BASIC_AUTH_USERNAME']
+    basic_auth_password = ENV['BASIC_AUTH_PASSWORD']
+    return if basic_auth_username.nil? || basic_auth_password.nil?
+    self.class.http_basic_authenticate_with name: basic_auth_username, password: basic_auth_password
+  end
 end
