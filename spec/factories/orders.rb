4414c0bd34ede423bb48ca2ecd72d1a7552bf71c
nagai kenji <nagai-kj@nagai-galaxy.com>
2019-05-12 09:54:01 +0900

    [fix]日付が変わると注文のテストが通らない問題

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :order do
-    delivery_date { '2019-05-01' }
+    delivery_date { DeliveryDateCalculation.new.dates.first }
     total_with_tax { 1000 }
     user { FactoryBot.create(:user, :with_delivery_info) }
     delivery_time_detail { FactoryBot.create(:delivery_time).delivery_time_details.first }
